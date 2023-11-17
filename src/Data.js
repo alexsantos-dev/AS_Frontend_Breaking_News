c191be40466ec8e8e3836172ad7c9ac1bc292f45
Alex Santos <alexsantos.web.developer@gmail.com>
2023-11-17 16:44:12 -0300

    organizando as pastas do react

---
+++ src/Data.js
@@ -0,0 +1,9 @@
+export const news = [
+    {
+        title: "News",
+        text: "Lorem ipsum dolor sit amet. Et suscipit quia in eveniet expedita aut placeat aliquid. 33 natus illo sed unde tenetur sed veritatis laborum ea obcaecati fugiat sit perferendis tempore eum placeat sequi eum quasi nisi. Sed quasi assumenda a eaque beatae et ullam adipisci ea exercitationem minima. Et aliquam magnam aut dolor velit ea vitae commodi ut itaque impedit eos quasi atque ut doloribus rerum qui consequatur rerum.",
+        image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.gettyimages.com.br%2Ffotos%2Fbreaking-news&psig=AOvVaw1tit9dapksGF955LnyR1ON&ust=1700335896587000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCPD_nYTjy4IDFQAAAAAdAAAAABAD",
+        likes: 21,
+        comments: 12
+    },
+]
