52eef838f3fdef5e058620ca3db33750ce106be4
Alex Santos <alexsantos.web.developer@gmail.com>
2023-11-21 11:23:22 -0300

    Bug do TopNewsService fixado

--- src/components/Card/Card.styles.jsx
+++ src/components/Card/Card.styles.jsx
@@ -4,7 +4,7 @@ export const CardContainer = styled.section`
   display: flex;
   flex-direction: column;
   gap: 1rem;
-  max-width: 100%;
+  width: 100%;
 
   box-shadow: rgba(50, 50, 105, 0.149) 0px 2px 5px 0px,
     rgba(0, 0, 0, 0.05) 0px 1px 1px 0px;
@@ -20,7 +20,7 @@ export const CardBody = styled.article`
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    padding: 2rem;
+    padding: 1rem;
     width: 100%;
   }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,15 +6,15 @@ import { getAllNews, getTopNews } from "../../services/news.services";
 import { HomeBody, HomeHeader } from "./Home.styles";
 
 export function Home() {
-  const [posts, setPosts] = useState([]);
+  const [news, setNews] = useState([]);
   const [topPost, setTopPost] = useState({});
 
   async function findPost() {
-    const postsResponse = await getAllNews();
-    setPosts(postsResponse.data.results);
+    const newsResponse = await getAllNews();
+    setNews(newsResponse.data.results);
 
-    const topPostResponse = await getTopNews();
-    setTopPost(topPostResponse.data.news);
+    const topNewsResponse = await getTopNews();
+    setTopPost(topNewsResponse.data.news);
   }
 
   useEffect(() => {
@@ -26,7 +26,7 @@ export function Home() {
       <Navbar />
       <HomeHeader>
         <Card
-          // top={true}
+          top={true}
           title={topPost.title}
           text={topPost.text}
           banner={topPost.banner}
@@ -35,20 +35,16 @@ export function Home() {
         />
       </HomeHeader>
       <HomeBody>
-        {posts.map(
-          (item, index) =>
-            // Verifica se o índice é maior que zero para não incluir o Card no HomeHeader
-            index > 0 && (
-              <Card
-                key={item.id}
-                title={item.title}
-                text={item.text}
-                banner={item.banner}
-                likes={item.likes}
-                comments={item.comments}
-              />
-            )
-        )}
+        {news.map((item) => (
+          <Card
+            key={item.id}
+            title={item.title}
+            text={item.text}
+            banner={item.banner}
+            likes={item.likes}
+            comments={item.comments}
+          />
+        ))}
       </HomeBody>
     </>
   );
