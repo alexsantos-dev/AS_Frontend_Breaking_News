69032abbce208add7da126e26a55755553d31b5f
Alex Santos <alexsantos.web.developer@gmail.com>
2023-11-23 10:37:23 -0300

    consumindo a api para pesquisas com o search

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,6 +1,44 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { searchNews } from "../../services/news.services";
+import { Card } from "../../components/Card/Card";
+import { SearchNews, ContainerResults } from "./Search.styles";
 
 export function Search() {
   const { title } = useParams();
-  return <h1>{title}</h1>;
+  const [post, setPost] = useState([]);
+
+  async function search() {
+    try {
+      const postApi = await searchNews(title);
+      setPost(postApi.data.findTitle);
+      console.log(postApi);
+    } catch (err) {
+      console.log(err);
+      setPost([]);
+    }
+  }
+
+  useEffect(() => {
+    search();
+  }, [title]);
+
+  return (
+    <>
+      <ContainerResults>
+        <SearchNews>
+          {post.map((item) => (
+            <Card
+              key={item.id}
+              title={item.title}
+              text={item.text}
+              banner={item.banner}
+              likes={item.likes}
+              comments={item.comments}
+            />
+          ))}
+        </SearchNews>
+      </ContainerResults>
+    </>
+  );
 }
---
+++ src/pages/Search/Search.styles.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ContainerResults = styled.section`
+  padding-top: 1rem;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const SearchNews = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 15px;
+  margin: 1rem auto;
+  width: 80%;
+`;
